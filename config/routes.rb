5f97f50078b25801fd0dc789c692eb53b6c634b4
hip750 <hajime.0608.pt@gmail.com>
2021-02-07 08:41:41 +0000

    Static Pages first setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root   'static_pages#home'
-  get    'static_pages/about' => 'static_pages#about'
+  get    '/about' => 'static_pages#about'
   get    '/terms_of_service' => 'static_pages#terms_of_service'
   get    '/privacy_policy' => 'static_pages#privacy_policy'
 end
--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -4,28 +4,28 @@ RSpec.describe "StaticPages", type: :request do
 
   describe "GET /home" do
     it "returns http success" do
-      get "/static_pages/home"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /about" do
     it "returns http success" do
-      get "/static_pages/about"
+      get "/about"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /terms_of_service" do
     it "returns http success" do
-      get "/static_pages/terms_of_service"
+      get "/terms_of_service"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /privacy_policy" do
     it "returns http success" do
-      get "/static_pages/privacy_policy"
+      get "/privacy_policy"
       expect(response).to have_http_status(:success)
     end
   end
