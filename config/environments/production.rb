9143af65ccf6e1a2de055f9ac155857e14580074
hip750 <hajime.0608.pt@gmail.com>
2021-03-07 14:27:17 +0000

    responsive layout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,17 @@ Rails.application.configure do
   config.force_ssl = true
 
   config.active_storage.service = :amazon
+
+  # mailer setting
+  config.action_mailer.default_url_options = { :host => 'glacial-falls-70520.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV['MAIL_ADDRESS'],
+    :password => ENV['APP_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
