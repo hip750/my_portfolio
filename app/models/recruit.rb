b663cb2c5d93750318ed424e948e1e3288396cfe
hip750 <hajime.0608.pt@gmail.com>
2021-03-08 15:40:48 +0000

    responsive layout

--- app/models/recruit.rb
+++ app/models/recruit.rb
@@ -1,6 +1,6 @@
 class Recruit < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode, if: :address_changed?
+  after_validation :geocode
 
   belongs_to :user
   has_many :likes, dependent: :destroy
