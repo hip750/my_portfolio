60cad1b7e8971b34575ce3010cd70056be78c409
hip750 <hajime.0608.pt@gmail.com>
2021-03-11 16:58:31 +0000

    git merge

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = true
+  config.eager_load = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
--- spec/requests/messages_request_spec.rb
+++ spec/requests/messages_request_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe MessageMailer, type: :request do
     end
 
     context 'when send_mail' do
-      it { expect(mail.from.first).to eq("from@example.com") }
+      it { expect(mail.from.first).to eq(ENV["MAIL_ADDRESS"]) }
       it { expect(mail.to.first).to eq(ENV["MAIL_ADDRESS"]) }
       it { expect(mail.subject).to eq("Medpot | 応募メール") }
       it { expect(mail.body).to match(/こちらはMedpodです。/) }
